625924a09c66859d2342706fed518e42089953f6
atemnov <Alexey.Temnov@bsc-ideas.com>
2018-03-25 15:27:06 +0300

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,24 @@
+
+function menuManipulator() {
+    console.log('menuManipulator');
+}
+
+function modalOpen() {
+    console.log('modalOpen');
+}
+
+function easyOnChange() {
+    console.log('easyOnChange');
+}
+
+function currChange() {
+    console.log('currChange');
+}
+
+function currSwap() {
+    console.log('currSwap');
+}
+
+function radialInit() {
+    console.log('radialInit');
+}
